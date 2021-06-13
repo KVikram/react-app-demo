ec1205620de4aa9bf278e9ebff7caec9500e5089
Vikram Kadiam <vikraal10@gmail.com>
2021-06-13 23:34:18 +0100

    feat(material):added material UI to the app

--- src/Tasks.js
+++ src/Tasks.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
+import { Button } from "@material-ui/core";
+import { Icon, TextField } from "@material-ui/core";
+
 const LOCAL_STORE_TODOS_KEY = "TODO_APP_TODOS_LIST";
 
 export default function Tasks() {
@@ -22,7 +25,7 @@ export default function Tasks() {
 
 	function addTodo(e) {
 		const name = todoNameRef.current.value;
-		if (name === "") return;
+		if (name === "" || name == null) return;
 		console.log(name);
 		let todo = {
 			id: new Date().getTime(),
@@ -53,9 +56,28 @@ export default function Tasks() {
 	return (
 		<div>
 			<TodoList todos={todos} toggleTodo={toggleTodo} />
-			<input type="text" ref={todoNameRef} onKeyUp={onEnter}></input>
-			<button onClick={addTodo}>Add</button>
-			<button onClick={clearTodo}>Clear</button>
+			{/* <input type="text" ref={todoNameRef} onKeyUp={onEnter}></input> */}
+			<TextField
+				id="outlined-basic"
+				type="text"
+				label="Task"
+				variant="outlined"
+				inputRef={todoNameRef}
+				onKeyUp={onEnter}
+			/>
+			{/* <button onClick={addTodo}>Add</button>
+			<button onClick={clearTodo}>Clear</button> */}
+			<Button variant="contained" color="primary" onClick={addTodo}>
+				Add
+			</Button>
+			<Button
+				variant="contained"
+				color="secondary"
+				onClick={clearTodo}
+				startIcon={<Icon>delete</Icon>}>
+				Clear
+			</Button>
+
 			<div>
 				{todos.filter((todo) => !todo.complete).length} Tasks left to do..
 			</div>
